2a1e25f2704cd0c8481257b819c13dd82f622e2c
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-10-23 22:07:53 +0530

    chore: image on signup & login

--- app/(auth)/signup/[[...rest]]/page.tsx
+++ app/(auth)/signup/[[...rest]]/page.tsx
@@ -2,6 +2,7 @@
 
 import { GoogleIcon, LoaderIcon } from '@/components/icons';
 import { signIn, useSession } from 'next-auth/react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
@@ -27,8 +28,9 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-[70vh] md:min-h-screen flex items-center justify-center px-7 sm:px-6 lg:px-48">
-      <div className="w-full space-y-8">
+    <div className="min-h-[70vh] md:min-h-screen flex flex-col md:flex-row items-center justify-center px-7 sm:px-6 lg:px-48 gap-8">
+      {/* Left: Signup Form */}
+      <div className="w-full md:w-1/2 space-y-8">
         <div className="text-left">
           <h1 className="text-3xl lg:text-[32px] font-semibold text-design-black mb-6 md:mb-4">
             Create your Account
@@ -63,6 +65,16 @@ export default function LoginPage() {
             or log in
           </Link>
         </div>
+      </div>
+      {/* Right: CV Home Image */}
+      <div className="hidden md:flex w-1/2 items-center justify-center">
+        <Image
+          src="/home/cv-home.png"
+          alt="CV Home Preview"
+          width={420}
+          height={420}
+          className="max-w-[420px] w-full h-auto rounded-xl"
+        />
       </div>
     </div>
   );
