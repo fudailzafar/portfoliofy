ad5a9d0036b755084c2e34bd50e9edaad8ef3c8d
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-11-07 23:02:39 +0530

    fix: update button text from 'Sign up' to 'Create Account'

--- app/(auth)/signup/[[...rest]]/page.tsx
+++ app/(auth)/signup/[[...rest]]/page.tsx
@@ -147,7 +147,7 @@ export default function SignupPage() {
                   <LoaderIcon />
                 </>
               ) : (
-                'Sign up'
+                'Create Account'
               )}
             </button>
           ) : (
